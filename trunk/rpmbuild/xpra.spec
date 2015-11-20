mjharkin/Xpra

commit 0b51b475486c80d567176212360bb7ec4130f027

use lockstep package version requirement for xpra-common
git-svn-id: https://xpra.org/svn/Xpra@11187 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/rpmbuild/xpra.spec
@@ -117,7 +117,7 @@ Requires: %{libvpx}
 Requires: %{libwebp}
 Requires: x264-xpra
 Requires: ffmpeg-xpra
-Requires: xpra-common
+Requires: xpra-common = %{build_no}%{dist}
 
 BuildRequires: pkgconfig
 BuildRequires: Cython
@@ -188,7 +188,7 @@ Requires: %{libvpx}
 Requires: %{libwebp}
 Requires: x264-xpra
 Requires: ffmpeg-xpra
-Requires: xpra-common
+Requires: xpra-common = %{build_no}%{dist}
 
 %description -n python3-xpra
 Xpra gives you "persistent remote applications" for X. That is, unlike normal X applications, applications run with xpra are "persistent" -- you can run them remotely, and they don't die if your connection does. You can detach them, and reattach them later -- even from another computer -- with no loss of state. And unlike VNC or RDP, xpra is for remote applications, not remote desktops -- individual applications show up as individual windows on your screen, managed by your window manager. They're not trapped in a box.

commit 2073b245f5968269015ad9190fe15877a9418085

fix breakage introduced in r11187: we must include the version in the version requirements (doh: who would have guessed..)
git-svn-id: https://xpra.org/svn/Xpra@11296 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/rpmbuild/xpra.spec
@@ -117,7 +117,7 @@ Requires: %{libvpx}
 Requires: %{libwebp}
 Requires: x264-xpra
 Requires: ffmpeg-xpra
-Requires: xpra-common = %{build_no}%{dist}
+Requires: xpra-common = %{version}-%{build_no}%{dist}
 
 BuildRequires: pkgconfig
 BuildRequires: Cython
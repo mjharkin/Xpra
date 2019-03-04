a3258e719be86f3705a2dc57442e594493efe4d0
Mark Harkin <mjharkin@gmail.com>
2019-03-04 13:00:16 +0100

    IE11 compat

--- trunk/src/html5/js/Client.js
+++ trunk/src/html5/js/Client.js
@@ -229,7 +229,7 @@ XpraClient.prototype.exc = function() {
 			//logging.ERROR = 40
 			this.send_log(40, [exception.stack]);
 		}
-		catch {
+		catch (e) {
 			//we tried our best
 		}
 	}
@@ -1017,7 +1017,7 @@ XpraClient.prototype._make_hello_base = function() {
 			}
 			this.debug("network", "digests:", digests);
 		}
-		catch {
+		catch (e) {
 			this.cerror("Error probing forge crypto digests");
 		}
 	}

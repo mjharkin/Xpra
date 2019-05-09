mjharkin/Xpra

commit 9d35e9a89887bfa460e988b45d29aff822727611

#1123 move kill_display to x11 server code
git-svn-id: https://xpra.org/svn/Xpra@22665 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/scripts/server.py
@@ -11,7 +11,6 @@
 import sys
 import os.path
 import atexit
-import signal
 import traceback
 
 from xpra.scripts.main import info, warn, no_gtk, validate_encryption, parse_env, configure_env
@@ -149,19 +148,6 @@ def close_gtk_display():
             displays = gdk.display_manager_get().list_displays()
         for d in displays:
             d.close()
-
-def kill_xvfb(xvfb_pid):
-    log = get_util_logger()
-    log.info("killing xvfb with pid %s", xvfb_pid)
-    try:
-        os.kill(xvfb_pid, signal.SIGTERM)
-    except OSError as e:
-        log.info("failed to kill xvfb process with pid %s:", xvfb_pid)
-        log.info(" %s", e)
-    from xpra.x11.vfb_util import PRIVATE_XAUTH
-    xauthority = os.environ.get("XAUTHORITY")
-    if PRIVATE_XAUTH and xauthority and os.path.exists(xauthority):
-        os.unlink(xauthority)
 
 
 def print_DE_warnings():
@@ -867,13 +853,6 @@ def do_run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=N
 
     if not proxying:
         add_cleanup(close_gtk_display)
-    if not proxying and not shadowing:
-        def kill_display():
-            if xvfb_pid:
-                kill_xvfb(xvfb_pid)
-        add_cleanup(kill_display)
-    else:
-        kill_display = None
 
     if opts.daemon:
         def noerr(fn, *args):
@@ -1048,6 +1027,8 @@ def do_run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=N
     try:
         app._ssl_wrap_socket = wrap_socket_fn
         app.init_dbus(dbus_pid, dbus_env)
+        if xvfb_pid or clobber:
+            app.init_display_pid(xvfb_pid)
         app.original_desktop_display = desktop_display
         app.exec_cwd = opts.chdir or cwd
         app.init(opts)
@@ -1058,11 +1039,13 @@ def do_run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=N
     except InitException as e:
         log.error("xpra server initialization error:")
         log.error(" %s", e)
+        app.cleanup()
         return 1
     except Exception as e:
         log.error("Error: cannot start the %s server", app.session_type, exc_info=True)
         log.error(str(e))
         log.info("")
+        app.cleanup()
         return 1
 
     #publish mdns records:
@@ -1090,32 +1073,18 @@ def do_run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=N
     app.init_when_ready(_when_ready)
 
     try:
-        #from here on, we own the vfb, even if we inherited one:
-        if (starting or starting_desktop or upgrading or upgrading_desktop):
-            if clobber:
-                #and it will be killed if exit cleanly:
-                xvfb_pid = get_xvfb_pid()
-            elif xvfb_pid:
-                save_xvfb_pid(xvfb_pid)
-
         log("running %s", app.run)
         r = app.run()
         log("%s()=%s", app.run, r)
     except KeyboardInterrupt:
         log.info("stopping on KeyboardInterrupt")
+        app.cleanup()
         return 0
     except Exception:
         log.error("server error", exc_info=True)
+        app.cleanup()
         return -128
     else:
         if r>0:
-            # Upgrading/exiting, so leave X and dbus servers running
-            if kill_display:
-                _cleanups.remove(kill_display)
-            from xpra.server import EXITING_CODE
-            if r==EXITING_CODE:
-                log.info("exiting: not cleaning up Xvfb")
-            else:
-                log.info("upgrading: not cleaning up Xvfb")
             r = 0
     return r

trunk/src/xpra/server/server_core.py
@@ -371,8 +371,8 @@ class ServerCore(object):
         self.idle_add(self.reset_server_timeout)
         self.idle_add(self.server_is_ready)
         self.do_run()
-        log("run() returning %s", self._upgrading)
-        return self._upgrading
+        log("run()")
+        return 0
 
     def server_is_ready(self):
         log.info("xpra is ready.")

trunk/src/xpra/server/server_base.py
@@ -97,6 +97,7 @@ class ServerBase(ServerBaseClass):
         self._authenticated_packet_handlers = {}
         self._authenticated_ui_packet_handlers = {}
 
+        self.display_pid = 0
         self._server_sources = {}
         self.client_properties = {}
         self.ui_driver = None
@@ -143,7 +144,6 @@ class ServerBase(ServerBaseClass):
         self.idle_timeout = opts.idle_timeout
         self.av_sync = opts.av_sync
         self.bandwidth_detection = opts.bandwidth_detection
-
 
     def setup(self):
         log("starting component init")

trunk/src/xpra/x11/vfb_util.py
@@ -298,6 +298,20 @@ def start_Xvfb(xvfb_str, pixel_depth, display_name, cwd, uid, gid, username, xau
     return xvfb, display_name, cleanups
 
 
+def kill_xvfb(xvfb_pid):
+    log = get_vfb_logger()
+    log.info("killing xvfb with pid %s", xvfb_pid)
+    import signal
+    try:
+        os.kill(xvfb_pid, signal.SIGTERM)
+    except OSError as e:
+        log.info("failed to kill xvfb process with pid %s:", xvfb_pid)
+        log.info(" %s", e)
+    xauthority = os.environ.get("XAUTHORITY")
+    if PRIVATE_XAUTH and xauthority and os.path.exists(xauthority):
+        os.unlink(xauthority)
+
+
 def set_initial_resolution(res=DEFAULT_VFB_RESOLUTION):
     try:
         log = get_vfb_logger()

trunk/src/xpra/x11/x11_server_base.py
@@ -59,6 +59,7 @@ class X11ServerBase(X11ServerCore):
         self.default_dpi = 0
         self._xsettings_manager = None
         self._xsettings_enabled = False
+        self.display_pid = 0
 
     def do_init(self, opts):
         X11ServerCore.do_init(self, opts)
@@ -68,6 +69,34 @@ class X11ServerBase(X11ServerCore):
             self._default_xsettings = XSettingsHelper().get_settings()
             log("_default_xsettings=%s", self._default_xsettings)
             self.init_all_server_settings()
+
+
+    def init_display_pid(self, pid):
+        if pid:
+            from xpra.scripts.server import _save_int
+            _save_int(b"_XPRA_SERVER_PID", pid)
+        elif self.clobber:
+            from xpra.scripts.server import _get_int
+            pid = _get_int(b"_XPRA_SERVER_PID")
+            log.info("xvfb pid=%i", pid)
+        self.display_pid = pid
+
+    def kill_display(self):
+        if self.display_pid:
+            from xpra.server import EXITING_CODE
+            if self._upgrading==EXITING_CODE:
+                log.info("exiting: not cleaning up Xvfb")
+            elif self._upgrading:
+                log.info("upgrading: not cleaning up Xvfb")
+            else:
+                from xpra.x11.vfb_util import kill_xvfb
+                kill_xvfb(self.display_pid)
+
+
+    def do_cleanup(self):
+        X11ServerCore.do_cleanup(self)
+        self.kill_display()
+
 
     def configure_best_screen_size(self):
         root_w, root_h = X11ServerCore.configure_best_screen_size(self)

commit 793adde88f372dc3ac051225d0fac3534efb6b18

#1123 move socket cleanup code to the server, fixes dangling sockets on exit caused by r22665
git-svn-id: https://xpra.org/svn/Xpra@22674 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/scripts/server.py
@@ -372,7 +372,8 @@ def add_mdns(mdns_recs, socktype, host_str, port):
 
 def create_sockets(opts, error_cb):
     from xpra.server.socket_util import (
-        parse_bind_ip, parse_bind_vsock, get_network_logger, setup_tcp_socket, setup_udp_socket, setup_vsock_socket,
+        parse_bind_ip, parse_bind_vsock, get_network_logger,
+        setup_tcp_socket, setup_udp_socket, setup_vsock_socket, setup_sd_listen_socket,
         )
     log = get_network_logger()
 
@@ -493,13 +494,13 @@ def create_sockets(opts, error_cb):
     if POSIX:
         try:
             from xpra.platform.xposix.sd_listen import get_sd_listen_sockets
-        except ImportError:
-            pass
+        except ImportError as e:
+            log("no systemd socket activation: %s", e)
         else:
             sd_sockets = get_sd_listen_sockets()
             log("systemd sockets: %s", sd_sockets)
             for stype, socket, addr in sd_sockets:
-                sockets.append((stype, socket, addr))
+                sockets.append(setup_sd_listen_socket(stype, socket, addr))
                 log("%s : %s", (stype, [addr]), socket)
                 if stype=="tcp":
                     host, iport = addr
@@ -961,10 +962,8 @@ def do_run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=N
     ssh_port = get_ssh_port()
     ssh_access = ssh_port>0 and opts.ssh.lower().strip() not in FALSE_OPTIONS
     for socktype, socket, sockpath, cleanup_socket in local_sockets:
-        #ie: ("unix-domain", sock, sockpath), cleanup_socket
-        sockets.append((socktype, socket, sockpath))
+        sockets.append((socktype, socket, sockpath, cleanup_socket))
         netlog("%s %s : %s", socktype, sockpath, socket)
-        add_cleanup(cleanup_socket)
         if opts.mdns and ssh_access:
             netlog("ssh %s:%s : %s", "", ssh_port, socket)
             add_mdns(mdns_recs, "ssh", "", ssh_port)
@@ -1010,6 +1009,7 @@ def do_run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=N
         app.init_virtual_devices(devices)
 
     try:
+        app.init_sockets(sockets)
         app._ssl_wrap_socket = wrap_socket_fn
         app.init_dbus(dbus_pid, dbus_env)
         if xvfb_pid or clobber:
@@ -1053,7 +1053,6 @@ def do_run_server(error_cb, opts, mode, xpra_file, extra_args, desktop_display=N
     del opts
 
     log("%s(%s)", app.init_sockets, sockets)
-    app.init_sockets(sockets)
     log("%s(%s)", app.init_when_ready, _when_ready)
     app.init_when_ready(_when_ready)
 

trunk/src/xpra/server/mixins/fileprint_server.py
@@ -49,7 +49,7 @@ class FilePrintServer(StubServerMixin):
 
     def init_sockets(self, sockets):
         #verify we have a local socket for printing:
-        unixsockets = [info for socktype, _, info in sockets if socktype=="unix-domain"]
+        unixsockets = [info for socktype, _, info, _ in sockets if socktype=="unix-domain"]
         printlog("local unix domain sockets we can use for printing: %s", unixsockets)
         if not unixsockets and self.file_transfer.printing:
             if not WIN32:

trunk/src/xpra/server/server_core.py
@@ -293,7 +293,7 @@ class ServerCore(object):
         return True
 
     def server_init(self):
-        pass
+        self.start_listen_sockets()
 
     def setup(self):
         self.init_packet_handlers()
@@ -397,6 +397,7 @@ class ServerCore(object):
         self.cleanup_protocols(protocols, reason, True)
         self._potential_protocols = []
         self.cleanup_udp_listeners()
+        self.cleanup_sockets()
         self.cleanup_dbus_server()
         if not self._upgrading:
             self.stop_dbus_server()
@@ -404,6 +405,17 @@ class ServerCore(object):
     def do_cleanup(self):
         #allow just a bit of time for the protocol packet flush
         sleep(0.1)
+
+
+    def cleanup_sockets(self):
+        si = self._socket_info
+        self._socket_info = ()
+        for socktype, _, info, cleanup in si:
+            log("cleanup_sockets() calling %s for %s %s", cleanup, socktype, info)
+            try:
+                cleanup()
+            except Exception:
+                log("cleanup error on %s", cleanup, exc_info=True)
 
 
     ######################################################################
@@ -676,9 +688,11 @@ class ServerCore(object):
     # sockets / connections / packets:
     def init_sockets(self, sockets):
         self._socket_info = sockets
+
+    def start_listen_sockets(self):
         ### All right, we're ready to accept customers:
-        for socktype, sock, info in sockets:
-            netlog("init_sockets(%s) will add %s socket %s (%s)", sockets, socktype, sock, info)
+        for socktype, sock, info, _ in self._socket_info:
+            netlog("init_sockets(%s) will add %s socket %s (%s)", self._socket_info, socktype, sock, info)
             self.socket_info[sock] = info
             self.idle_add(self.add_listen_socket, socktype, sock)
             if socktype=="unix-domain" and info:
@@ -1909,7 +1923,7 @@ class ServerCore(object):
 
     def get_socket_info(self):
         si = {}
-        for socktype, _, info in self._socket_info:
+        for socktype, _, info, _ in self._socket_info:
             if info:
                 si.setdefault(socktype, {}).setdefault("listeners", []).append(info)
         for socktype, auth_classes in self.auth_classes.items():

trunk/src/xpra/server/socket_util.py
@@ -128,12 +128,11 @@ def setup_tcp_socket(host, iport, socktype="tcp"):
             tcp_socket.close()
         except (OSError, IOError):
             pass
-    add_cleanup(cleanup_tcp_socket)
     if iport==0:
         iport = tcp_socket.getsockname()[1]
         log.info("allocated %s port %i on %s", socktype, iport, host)
     log("%s: %s:%s : %s", socktype, host, iport, socket)
-    return socktype, tcp_socket, (host, iport)
+    return socktype, tcp_socket, (host, iport), cleanup_tcp_socket
 
 def create_udp_socket(host, iport):
     if host.find(":")<0:
@@ -160,12 +159,11 @@ def setup_udp_socket(host, iport, socktype="udp"):
             udp_socket.close()
         except (OSError, IOError):
             pass
-    add_cleanup(cleanup_udp_socket)
     if iport==0:
         iport = udp_socket.getsockname()[1]
         log.info("allocated UDP port %i for %s", iport, host)
     log("%s: %s:%s : %s", socktype, host, iport, socket)
-    return socktype, udp_socket, (host, iport)
+    return socktype, udp_socket, (host, iport), cleanup_udp_socket
 
 
 def parse_bind_ip(bind_ip, default_port=DEFAULT_PORT):
@@ -203,8 +201,7 @@ def setup_vsock_socket(cid, iport):
             vsock_socket.close()
         except (OSError, IOError):
             pass
-    add_cleanup(cleanup_vsock_socket)
-    return "vsock", vsock_socket, (cid, iport)
+    return "vsock", vsock_socket, (cid, iport), cleanup_vsock_socket
 
 def parse_bind_vsock(bind_vsock):
     vsock_sockets = set()
@@ -213,6 +210,16 @@ def parse_bind_vsock(bind_vsock):
         for spec in bind_vsock:
             vsock_sockets.add(parse_vsock(spec))
     return vsock_sockets
+
+def setup_sd_listen_socket(stype, sock, addr):
+    log = get_network_logger()
+    def cleanup_sd_listen_socket():
+        log.info("closing sd listen socket %s", addr)
+        try:
+            sock.close()
+        except (OSError, IOError):
+            pass
+    return stype, sock, addr, cleanup_sd_listen_socket
 
 
 def normalize_local_display_name(local_display_name):

trunk/src/xpra/x11/x11_server_core.py
@@ -104,6 +104,7 @@ class X11ServerCore(GTKServerBase):
         if server_features.windows:
             from xpra.x11.x11_window_filters import init_x11_window_filters
             init_x11_window_filters()
+        super(X11ServerCore, self).server_init()
 
     def do_init(self, opts):
         self.randr = opts.resize_display
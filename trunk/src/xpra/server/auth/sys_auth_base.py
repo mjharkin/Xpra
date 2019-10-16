mjharkin/Xpra

commit 2c288072c847a6e85d63a62441ab884b22c5f4da

fix minor warnings
git-svn-id: https://xpra.org/svn/Xpra@22690 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/server/auth/sys_auth_base.py
@@ -30,7 +30,7 @@ def parse_uid(v):
     if v:
         try:
             return int(v)
-        except:
+        except TypeError:
             log("uid '%s' is not an int", v)
     if POSIX:
         try:
@@ -46,7 +46,7 @@ def parse_gid(v):
     if v:
         try:
             return int(v)
-        except:
+        except TypeError:
             log("gid '%s' is not an int", v)
     if POSIX:
         try:
@@ -96,7 +96,7 @@ class SysAuthenticatorBase(object):
         return self.salt, self.digest
 
     def get_passwords(self):
-        p = self.get_password()
+        p = self.get_password()     #pylint: disable=assignment-from-none
         if p is not None:
             return (p,)
         return ()

trunk/src/xpra/server/rfb/rfb_server.py
@@ -121,7 +121,7 @@ class RFBServer(object):
         start_refresh = getattr(self, "start_refresh", None)
         if start_refresh:
             for wid in tuple(self._window_to_id.values()):
-                start_refresh(wid)
+                start_refresh(wid)      #pylint: disable=not-callable
 
     def _process_rfb_PointerEvent(self, _proto, packet):
         if not server_features.input_devices or self.readonly:

trunk/src/xpra/server/shadow/gtk_shadow_server_base.py
@@ -51,7 +51,7 @@ class GTKShadowServerBase(ShadowServerBase, GTKServerBase):
     def cleanup(self):
         self.cleanup_tray()
         ShadowServerBase.cleanup(self)
-        GTKServerBase.cleanup(self)
+        GTKServerBase.cleanup(self)     #@UndefinedVariable
 
 
     def client_startup_complete(self, ss):

trunk/src/xpra/server/shadow/shadow_server_base.py
@@ -79,7 +79,7 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
 
 
     def guess_session_name(self, _procs=None):
-        self.session_name = get_wm_name()
+        self.session_name = get_wm_name()       # pylint: disable=assignment-from-none
 
     def get_server_mode(self):
         if PYTHON3:
@@ -172,7 +172,7 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
         #directly on the screen we shadow
         notifylog("notify_new_user(%s) notifier=%s", ss, self.notifier)
         if self.notifier:
-            tray = self.get_notification_tray()
+            tray = self.get_notification_tray()     #pylint: disable=assignment-from-none
             nid = 0
             title = "User '%s' connected to the session" % (ss.name or ss.username or ss.uuid)
             body = "\n".join(ss.get_connect_info())
@@ -284,7 +284,7 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
 
     def poll_cursor(self):
         prev = self.last_cursor_data
-        curr = self.do_get_cursor_data()
+        curr = self.do_get_cursor_data()        #pylint: disable=assignment-from-none
         self.last_cursor_data = curr
         def cmpv(lcd):
             if not lcd:
@@ -297,8 +297,8 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
             fields = ("x", "y", "width", "height", "xhot", "yhot", "serial", "pixels", "name")
             if len(prev or [])==len(curr or []) and len(prev or [])==len(fields):
                 diff = []
-                for i in range(len(prev)):
-                    if prev[i]!=curr[i]:
+                for i, prev_value in enumerate(prev):
+                    if prev_value!=curr[i]:
                         diff.append(fields[i])
                 cursorlog("poll_cursor() attributes changed: %s", diff)
             if SAVE_CURSORS and curr:
@@ -342,7 +342,7 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
     def parse_screen_info(self, ss):
         try:
             log.info(" client root window size is %sx%s", *ss.desktop_size)
-        except:
+        except Exception:
             log.info(" unknown client desktop size")
         return self.get_root_window_size()
 
@@ -355,7 +355,7 @@ class ShadowServerBase(SHADOWSERVER_BASE_CLASS):
 
     def set_keyboard_repeat(self, key_repeat):
         """ don't override the existing desktop """
-        pass
+        pass    #pylint: disable=unnecessary-pass
 
     def set_keymap(self, server_source, force=False):
         log("set_keymap%s", (server_source, force))

trunk/src/xpra/server/source/encodings_mixin.py
@@ -149,7 +149,7 @@ class EncodingsMixin(StubSourceMixin):
             self.calculate_window_ids.remove(wid)
             try:
                 del self.calculate_window_pixels[wid]
-            except:
+            except KeyError:
                 pass
             ws = self.window_sources.get(wid)
             if ws is None:
@@ -160,7 +160,7 @@ class EncodingsMixin(StubSourceMixin):
                                          len(fullscreen_wids)>0 and wid not in fullscreen_wids,
                                          len(maximized_wids)>0 and wid not in maximized_wids)
                 ws.reconfigure()
-            except:
+            except Exception:
                 log.error("error on window %s", wid, exc_info=True)
             if self.is_closed():
                 return
@@ -330,7 +330,7 @@ class EncodingsMixin(StubSourceMixin):
             #enabling video proxy:
             try:
                 self.parse_proxy_video()
-            except:
+            except Exception:
                 proxylog.error("failed to parse proxy video", exc_info=True)
 
         sc = self.encoding_options.get("scaling.control", self.scaling_control)

trunk/src/xpra/server/source/networkstate_mixin.py
@@ -7,7 +7,7 @@
 import os
 
 from xpra.util import envbool, envint, CLIENT_PING_TIMEOUT
-from xpra.os_util import monotonic_time
+from xpra.os_util import monotonic_time, POSIX
 from xpra.server.source.stub_source_mixin import StubSourceMixin
 from xpra.log import Logger
 
@@ -46,7 +46,8 @@ class NetworkStateMixin(StubSourceMixin):
         import time
         self.send_async("ping", now_ms, int(time.time()*1000), will_have_more=False)
         timeout = PING_TIMEOUT
-        self.check_ping_echo_timers[now_ms] = self.timeout_add(timeout*1000, self.check_ping_echo_timeout, now_ms, timeout)
+        self.check_ping_echo_timers[now_ms] = self.timeout_add(timeout*1000,
+                                                               self.check_ping_echo_timeout, now_ms, timeout)
 
     def check_ping_echo_timeout(self, now_ms, timeout):
         try:
@@ -67,11 +68,9 @@ class NetworkStateMixin(StubSourceMixin):
         cl = -1
         if PING_DETAILS:
             #send back the load average:
-            try:
-                (fl1, fl2, fl3) = os.getloadavg()
+            if POSIX:
+                fl1, fl2, fl3 = os.getloadavg()
                 l1,l2,l3 = int(fl1*1000), int(fl2*1000), int(fl3*1000)
-            except:
-                l1,l2,l3 = 0,0,0
             #and the last client ping latency we measured (if any):
             if self.statistics.client_ping_latency:
                 _, cl = self.statistics.client_ping_latency[-1]

trunk/src/xpra/server/source/webcam_mixin.py
@@ -52,7 +52,7 @@ class WebcamMixin(StubSourceMixin):
         self.stop_all_virtual_webcams()
 
 
-    def get_info(self, _proto=None):
+    def get_info(self):
         return {
             "webcam" : {
                 "encodings"         : self.webcam_encodings,

trunk/src/xpra/server/window/motion.pyx
@@ -18,7 +18,7 @@ from xpra.util import envbool, repr_ellipsized, csv
 from xpra.log import Logger
 log = Logger("encoding", "scroll")
 
-from xpra.buffers.membuf cimport memalign, object_as_buffer, xxh64
+from xpra.buffers.membuf cimport memalign, object_as_buffer, xxh64      #pylint: disable=syntax-error
 from xpra.rectangle import rectangle
 
 

commit 6e499d45aa0bde566bcec384187c953e932db8e8

fix bug introduced by r22690: exception type was now too strict
git-svn-id: https://xpra.org/svn/Xpra@24152 3bb7dfac-3a0b-4e04-842a-767bc560f471

trunk/src/xpra/server/auth/sys_auth_base.py
@@ -23,7 +23,7 @@ def parse_uid(v) -> int:
     if v:
         try:
             return int(v)
-        except TypeError:
+        except (TypeError, ValueError):
             log("uid '%s' is not an int", v)
     if POSIX:
         try:
@@ -39,7 +39,7 @@ def parse_gid(v) -> int:
     if v:
         try:
             return int(v)
-        except TypeError:
+        except (TypeError, ValueError):
             log("gid '%s' is not an int", v)
     if POSIX:
         try: